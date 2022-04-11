7ed2084ee754739fd1f0adb384a360c4c64b8921
smit1298 <61621188+smit1298@users.noreply.github.com>
2022-04-11 13:24:05 +0100

    creating modules
    created a module, removed the function replaceTemplate from index.js  and use it as a modules file in the modules folder

--- .history/index_20220411130420.js
+++ .history/index_20220411130420.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-const replaceTemplate = require('./modules/replaceTemplate');
 
 // /////////////////////
 // FILES
